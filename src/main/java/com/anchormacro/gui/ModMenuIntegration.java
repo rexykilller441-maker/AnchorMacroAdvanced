2ff4127d1c6e2adf9cadf9b1b122986361f44d09
rexykilller441-maker <rexykilller441@gmail.com>
2025-11-23 01:57:06 +0530

    Update ModMenuIntegration.java

--- src/main/java/com/anchormacro/gui/ModMenuIntegration.java
+++ src/main/java/com/anchormacro/gui/ModMenuIntegration.java
@@ -1,12 +1,11 @@
 package com.anchormacro.gui;
 
 import com.anchormacro.AnchorMacroAdvanced;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
-import me.shedaniel.modmenu.api.ConfigScreenFactory;
-import me.shedaniel.modmenu.api.ModMenuApi;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 public class ModMenuIntegration implements ModMenuApi {
@@ -16,26 +15,79 @@ public class ModMenuIntegration implements ModMenuApi {
         return parent -> {
             ConfigBuilder builder = ConfigBuilder.create()
                     .setParentScreen(parent)
-                    .setTitle(Text.of("AnchorMacroAdvanced Settings"));
+                    .setTitle(Text.literal("AnchorMacroAdvanced Settings"));
 
             ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-            ConfigCategory general = builder.getOrCreateCategory(Text.of("General"));
+            ConfigCategory general = builder.getOrCreateCategory(Text.literal("General"));
 
-            general.addEntry(entryBuilder.startBooleanToggle("Enabled", AnchorMacroAdvanced.config.enabled)
-                    .setSaveConsumer(val -> { AnchorMacroAdvanced.config.enabled = val; AnchorMacroAdvanced.config.save(); })
+            general.addEntry(entryBuilder.startBooleanToggle(Text.literal("Enabled"), AnchorMacroAdvanced.config.enabled)
+                    .setDefaultValue(false)
+                    .setSaveConsumer(val -> { 
+                        AnchorMacroAdvanced.config.enabled = val; 
+                        AnchorMacroAdvanced.config.save(); 
+                    })
                     .build());
 
-            general.addEntry(entryBuilder.startBooleanToggle("Safe Anchor", AnchorMacroAdvanced.config.safeAnchor)
-                    .setSaveConsumer(val -> { AnchorMacroAdvanced.config.safeAnchor = val; AnchorMacroAdvanced.config.save(); })
+            general.addEntry(entryBuilder.startBooleanToggle(Text.literal("Safe Anchor"), AnchorMacroAdvanced.config.safeAnchor)
+                    .setDefaultValue(true)
+                    .setSaveConsumer(val -> { 
+                        AnchorMacroAdvanced.config.safeAnchor = val; 
+                        AnchorMacroAdvanced.config.save(); 
+                    })
                     .build());
 
-            general.addEntry(entryBuilder.startIntSlider("Action Delay", AnchorMacroAdvanced.config.actionDelay, 0, 10)
-                    .setSaveConsumer(val -> { AnchorMacroAdvanced.config.actionDelay = val; AnchorMacroAdvanced.config.save(); })
+            general.addEntry(entryBuilder.startBooleanToggle(Text.literal("AutoSearch"), AnchorMacroAdvanced.config.autoSearch)
+                    .setDefaultValue(false)
+                    .setSaveConsumer(val -> { 
+                        AnchorMacroAdvanced.config.autoSearch = val; 
+                        AnchorMacroAdvanced.config.save(); 
+                    })
                     .build());
 
-            general.addEntry(entryBuilder.startBooleanToggle("AutoSearch", AnchorMacroAdvanced.config.autoSearch)
-                    .setSaveConsumer(val -> { AnchorMacroAdvanced.config.autoSearch = val; AnchorMacroAdvanced.config.save(); })
+            general.addEntry(entryBuilder.startIntSlider(Text.literal("Action Delay"), AnchorMacroAdvanced.config.actionDelay, 0, 10)
+                    .setDefaultValue(2)
+                    .setSaveConsumer(val -> { 
+                        AnchorMacroAdvanced.config.actionDelay = val; 
+                        AnchorMacroAdvanced.config.save(); 
+                    })
                     .build());
+
+            general.addEntry(entryBuilder.startIntSlider(Text.literal("Anchor Slot"), AnchorMacroAdvanced.config.anchorSlot, 1, 9)
+                    .setDefaultValue(1)
+                    .setSaveConsumer(val -> { 
+                        AnchorMacroAdvanced.config.anchorSlot = val; 
+                        AnchorMacroAdvanced.config.save(); 
+                    })
+                    .build());
+
+            general.addEntry(entryBuilder.startIntSlider(Text.literal("Glowstone Slot"), AnchorMacroAdvanced.config.glowstoneSlot, 1, 9)
+                    .setDefaultValue(2)
+                    .setSaveConsumer(val -> { 
+                        AnchorMacroAdvanced.config.glowstoneSlot = val; 
+                        AnchorMacroAdvanced.config.save(); 
+                    })
+                    .build());
+
+            general.addEntry(entryBuilder.startIntSlider(Text.literal("Totem Slot"), AnchorMacroAdvanced.config.totemSlot, 1, 9)
+                    .setDefaultValue(9)
+                    .setSaveConsumer(val -> { 
+                        AnchorMacroAdvanced.config.totemSlot = val; 
+                        AnchorMacroAdvanced.config.save(); 
+                    })
+                    .build());
+
+            general.addEntry(entryBuilder.startStrField(Text.literal("Mode"), AnchorMacroAdvanced.config.mode)
+                    .setDefaultValue("normal")
+                    .setSaveConsumer(val -> { 
+                        AnchorMacroAdvanced.config.mode = val; 
+                        AnchorMacroAdvanced.config.save(); 
+                    })
+                    .setTooltip(Text.literal("Options: normal, double, or human"))
+                    .build());
+
+            builder.setSavingRunnable(() -> {
+                AnchorMacroAdvanced.config.save();
+            });
 
             return builder.build();
         };
