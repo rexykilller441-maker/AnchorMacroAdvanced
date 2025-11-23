a2a004735b8363fcb91d7208a4a958a11a8c8e86
rexykilller441-maker <rexykilller441@gmail.com>
2025-11-23 10:30:00 +0530

    Update ItemUtils.java

--- src/main/java/com/anchormacro/util/ItemUtils.java
+++ src/main/java/com/anchormacro/util/ItemUtils.java
@@ -4,6 +4,7 @@ import com.anchormacro.AnchorMacroAdvanced;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 
@@ -27,15 +28,15 @@ public class ItemUtils {
 
     public static int findHotbarItem(Item item) {
         for (int i = 0; i < 9; i++) {
-            if (mc.player.inventory.getStack(i).getItem() == item) return i;
+            if (mc.player.getInventory().getStack(i).getItem() == item) return i;
         }
         return -1;
     }
 
     public static void placeBlockAtCursor(Item item) {
         // Simplified, normally send right-click packet / use client interaction
-        mc.interactionManager.interactBlock(mc.player, mc.world, Hand.MAIN_HAND,
-                new BlockHitResult(mc.player.getPos(), mc.player.getHorizontalFacing(), new BlockPos(mc.player.getPos()), false));
+        mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND,
+                new BlockHitResult(mc.player.getPos(), mc.player.getHorizontalFacing(), BlockPos.ofFloored(mc.player.getPos()), false));
     }
 
     public static void useItemOnBlock(Item item) {
